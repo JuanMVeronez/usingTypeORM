94d901ea69ed3a7d4215d5da2d4b3b04d0fbbcdd
Juan Veronez <juan.monteirov@gmail.com>
2021-08-19 12:48:45 -0300

    typescript, ts-node-dev and tspaths add on project

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  
+  
+  res.send("It's ok!")
+})
+
+app.listen(3333, () => {
+  console.log('Server Working 🔥')
+})
